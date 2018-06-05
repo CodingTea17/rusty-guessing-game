f2b30382a646c8fec456da0cc0b19569ec40572d
Dawson Mortenson <dawson.mortenson@gmail.com>
2018-06-05 16:56:48 -0700

    add error handling for user input and finishing touches

--- src/main.rs
+++ src/main.rs
@@ -10,23 +10,30 @@ fn main() {
   // Generate a random num using the rand crate
   let number_to_guess = rand::thread_rng().gen_range(1, 101);
 
-  println!("The secret number is: {}", number_to_guess);
+  // println!("The secret number is: {}", number_to_guess);
 
-  println!("Input your guess:");
+   loop {
+      println!("Input your guess:");
 
-  let mut guess = String::new();
+      let mut guess = String::new();
 
-  io::stdin().read_line(&mut guess)
-    .expect("Failed to read line");
+      io::stdin().read_line(&mut guess)
+        .expect("Failed to read line");
 
-  let guess: u32 = guess.trim().parse()
-    .expect("Please type a number!");
+      let guess: u32 = match guess.trim().parse() {
+          Ok(num) => num,
+          Err(_) => continue,
+      };
 
-  println!("You guess: {}", guess);
+      println!("You guess: {}", guess);
 
-  match guess.cmp(&number_to_guess) {
-      Ordering::Less => println!("Too small!"),
-      Ordering::Greater => println!("Too big!"),
-      Ordering::Equal => println!("You win!"),
-  }
+      match guess.cmp(&number_to_guess) {
+          Ordering::Less => println!("Too small!"),
+          Ordering::Greater => println!("Too big!"),
+          Ordering::Equal => {
+              println!("You win!");
+              break;
+          }
+      }
+   }
 }
